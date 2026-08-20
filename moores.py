3c482aadeb33a669f7e5be25dc583de7e604e33b
hannaquintanaw-hue <hannaquintana.w@gmail.com>
2026-08-20 14:56:34 +0000

    Transistors

---
+++ moores.py
@@ -0,0 +1,10 @@
+def main():
+
+    Transistors= 17800000000
+    Years=int(input("How many more years into the future?"))
+    Transistors*=round(2**(years/2))
+    print(f"{Transistors:,}")
+
+    if_name_=="_main_":
+    main()
+    
