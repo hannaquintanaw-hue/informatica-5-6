863e307dbd8d4f8d0ac8a388a77a960ac39b10c8
hannaquintanaw-hue <hannaquintana.w@gmail.com>
2026-08-27 14:30:38 +0000

    fgf

--- if-exercises.py
+++ if-exercises.py
@@ -3,6 +3,23 @@ def main():
         if integer < 0:
                 print(integer * -1)
         else
+if number < 0:
+        print(number*-1)
+
+else:
+        print(number)
+print("Intput Calculator")
+number 1 = float(input("Select fisrt numer"))
+
+
+if opperation == "add":
+        print(number1 + number2)
+
+elif oppperation == "subtract":
+        print(number1 - number2)
+
+else:
+        print
 
 
 
