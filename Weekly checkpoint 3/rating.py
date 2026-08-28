1894ee1f794056c56a317208a705007a87050d76
hannaquintanaw-hue <hannaquintana.w@gmail.com>
2026-08-28 21:56:56 +0000

    ajdik

--- Weekly checkpoint 3/rating.py
+++ Weekly checkpoint 3/rating.py
@@ -1,7 +1,21 @@
 def main()
      print("Welcome to chimichangas")
-     rating = int(input("Rate chimichangas:"))
-     
+     rating = float(input("Rate chimichangas:"))
+
+    if 5.1 > rating > 4.4:
+            print("Perfection.")
+    elif 4.5 > rating > 4:
+            print(Excellent")
+    elif 4 > rating > 3:
+                print("Good")
+    elif 3 > rating > 2:
+                print("Fairt")
+    elif 2 > rating > 0:
+                print("Poor")
+    else:
+    print("invalid response")
+
+
 
 
 
