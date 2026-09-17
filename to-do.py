f377ffbb31beba9fff11d0f2aa587e4c759a7a2d
hannaquintanaw-hue <hannaquintana.w@gmail.com>
2026-09-17 15:33:41 +0000

    ggg

---
+++ to-do.py
@@ -0,0 +1,17 @@
+def main():
+    tasks = []
+    command = ""
+    while True:
+        print(f"Tasks to do: {len(tasks)}")
+        print(tasks)
+        command = input("What do you want to do? (add, complete, exit): ")
+        if command == "add":
+            new_task = input("Enter new task: ")
+            tasks.append(new_task)
+        elif command == "complete":
+            
+
+
+
+if __name__ == "__main__":
+    main()
